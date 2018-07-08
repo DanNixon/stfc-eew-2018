29db1f479e45fe1d666f6bc8bab019abf66d2972
Dan Nixon <dan@dan-nixon.com>
2018-07-08 13:25:03 +0100

    Add switch and button code samples

---
+++ code_samples/mcu/single_button/src/main.ino
@@ -0,0 +1,37 @@
+#include <Streaming.h>
+
+const int button_pin = 15;
+
+volatile unsigned long last_change_time = 0;
+volatile bool button_pushed = false;
+
+unsigned int times_pressed = 0;
+
+void handle_interrupt()
+{
+  const auto now = millis();
+
+  if (now - last_change_time > 500)
+  {
+    button_pushed = true;
+    last_change_time = now;
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(button_pin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(button_pin), handle_interrupt, FALLING);
+}
+
+void loop() {
+  if (button_pushed)
+  {
+    button_pushed = false;
+
+    Serial << "Button was pushed " << ++times_pressed << " time(s)\n";
+  }
+
+  delay(10);
+}
---
+++ code_samples/mcu/single_switch/src/main.ino
@@ -0,0 +1,36 @@
+#include <Streaming.h>
+
+const int switch_pin = 15;
+
+volatile unsigned long last_change_time = 0;
+volatile bool switch_changed = false;
+
+void handle_interrupt()
+{
+  const auto now = millis();
+
+  if (now - last_change_time > 100)
+  {
+    switch_changed = true;
+    last_change_time = now;
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(switch_pin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(switch_pin), handle_interrupt, CHANGE);
+}
+
+void loop() {
+  if (switch_changed)
+  {
+    switch_changed = false;
+
+    const auto state = digitalRead(switch_pin);
+    Serial << "Switch state is now " << state << '\n';
+  }
+
+  delay(10);
+}
