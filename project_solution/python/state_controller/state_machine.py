8149ac03b9ecd2d9d648f5487722fd552bfd654b
Dan Nixon <dan@dan-nixon.com>
2018-07-12 16:35:46 +0100

    Add complete-ish FSM

--- project_solution/python/state_controller/state_machine.py
+++ project_solution/python/state_controller/state_machine.py
@@ -11,14 +11,13 @@ class SafetySystem(object):
         State(name='button_2_waiting', on_enter=['on_enter_button_2_waiting']),
         State(name='button_3_waiting', on_enter=['on_enter_button_3_waiting']),
         State(name='key_rack_waiting', on_enter=['on_enter_key_rack_waiting']),
-        State(name='door_closing', on_enter=['on_enter_door_closing']),
+        State(name='key_rack_full', on_enter=['on_enter_key_rack_full']),
         State(name='door_closed',on_enter=['on_enter_door_closed']),
-        State(name='searched_and_locked',on_enter=['on_enter_searched_and_locked']),
-        State(name='door_opening', on_enter=['on_enter_door_opening']),
-        State(name='door_open', on_enter=['on_enter_door_open']),
-        State(name='shutter_closing',on_enter=['on_enter_shutter_closing']),
-        State(name='ready', on_enter=['on_enter_ready']),
-        State(name='shutter_open', on_enter=['on_enter_shutter_open']),
+        State(name='door_locking',on_enter=['on_enter_door_locking']),
+        State(name='door_locked',on_enter=['on_enter_door_locked']),
+        State(name='shutter_open',on_enter=['on_enter_shutter_open']),
+        State(name='shutter_closed',on_enter=['on_enter_shutter_closed']),
+        State(name='door_unlocking',on_enter=['on_enter_door_unlocking']),
         State(name='error',on_enter=['on_enter_error']),
     ]
 
@@ -32,19 +31,19 @@ class SafetySystem(object):
         )
 
         self.machine.add_transition(
-            trigger='press_button1',
+            trigger='press_search_point_1_button',
             source=['button_1_waiting'],
             dest='button_2_waiting'
         )
 
         self.machine.add_transition(
-            trigger='press_button2',
+            trigger='press_search_point_2_button',
             source=['button_2_waiting'],
             dest='button_3_waiting'
         )
 
         self.machine.add_transition(
-            trigger='press_button3',
+            trigger='press_search_point_3_button',
             source=['button_3_waiting'],
             dest='key_rack_waiting'
         )
@@ -52,67 +51,61 @@ class SafetySystem(object):
         self.machine.add_transition(
             trigger='key_rack_full',
             source=['key_rack_waiting'],
-            dest='door_closing'
+            dest='key_rack_full'
         )
 
         self.machine.add_transition(
-            trigger='closed_limit_switch',
-            source='door_closing',
+            trigger='key_rack_not_full',
+            source=['key_rack_full'],
+            dest='button_1_waiting'
+        )
+
+        self.machine.add_transition(
+            trigger='door_is_closed',
+            source=['key_rack_full'],
             dest='door_closed'
         )
 
         self.machine.add_transition(
-            trigger='door_locked',
-            source='door_closed',
-            dest='searched_and_locked'
-        )
-
-        self.machine.add_transition(
-            trigger='open_door',
-            source=['door_closed', 'door_closing', 'searched_and_locked'],
-            dest='door_opening'
-        )
-
-        self.machine.add_transition(
-            trigger='closed_limit_switch',
-            source=['door_opening','door_open'],
-            dest='error'
-        )
-
-        self.machine.add_transition(
-            trigger='open_limit_switch',
-            source='door_opening',
-            dest='door_open'
-        )
-
-        self.machine.add_transition(
-            trigger='open_limit_switch',
-            source=['door_closing', 'door_closed'],
-            dest='error'
-        )
-
-        self.machine.add_transition(
-            trigger='safety_edge_hit',
-            source=['door_opening', 'door_closing'],
-            dest='error'
-        )
-
-        self.machine.add_transition(
-            trigger='close_shutter',
-            source=['searched_and_locked', 'shutter_open'],
-            dest='shutter_closing'
-        )
-
-        self.machine.add_transition(
-            trigger='shutter_closed',
-            source=['shutter_closing'],
-            dest='ready'
+            trigger='door_is_opening',
+            source=['door_closed'],
+            dest='button_1_waiting'
         )
 
         self.machine.add_transition(
             trigger='open_shutter',
-            source=['shutter_closing','ready'],
+            source=['door_closed'],
+            dest='door_locking'
+        )
+
+        self.machine.add_transition(
+            trigger='doot_is_locked',
+            source=['door_locking'],
+            dest='door_locked'
+        )
+
+        self.machine.add_transition(
+            trigger='',
+            source=['door_locked'],
             dest='shutter_open'
+        )
+
+        self.machine.add_transition(
+            trigger='close_shutter',
+            source=['shutter_open'],
+            dest='shutter_closed'
+        )
+
+        self.machine.add_transition(
+            trigger='',
+            source=['shutter_closed'],
+            dest='door_unlocking'
+        )
+
+        self.machine.add_transition(
+            trigger='door_is_unlocked',
+            source=['door_unlocking'],
+            dest='door_closed'
         )
 
         self.machine.add_transition(
@@ -126,11 +119,11 @@ class SafetySystem(object):
         node_type, node_id, command, payload = msg
 
         if (node_type == DeviceType.SEARCH_POINT and node_id == 0):
-            self.press_button1()
+            self.press_search_point_1_button()
         elif (node_type == DeviceType.SEARCH_POINT and node_id == 1):
-            self.press_button2()
+            self.press_search_point_2_button()
         elif (node_type == DeviceType.SEARCH_POINT and node_id == 2):
-            self.press_button3()
+            self.press_search_point_3_button()
 
         # TODO
 
@@ -149,35 +142,33 @@ class SafetySystem(object):
         self.device_group.send_message(DeviceType.SEARCH_POINT, 2, Command.SET_LED, b'\x00\xff\x00')
         self.device_group.send_message(DeviceType.KEY_RACK, 0, Command.SET_LED, b'\xff\xff\x00')
 
-    def on_enter_door_closing(self):
-        print('Change key rack LED green - door is closing')
+    def on_enter_key_rack_full(self):
+        print("Key rack light green")
         # TODO
 
     def on_enter_door_closed(self):
-        print('Door closed, confirm locked')
+        pass
         # TODO
 
-    def on_enter_searched_and_locked(self):
-        print('Door locked, ready to close shutter')
+    def on_enter_door_locking(self):
+        pass
         # TODO
 
-    def on_enter_door_opening(self):
-        print('The door is opening')
+    def on_enter_door_locked(self):
+        pass
         # TODO
-
-    def on_enter_door_open(self):
-        print('The door is open')
-        # TODO
-
-    def on_enter_shutter_closing(self):
-        print('Please confirm when the shutter is closed')
-        # TODO
-
-    def on_enter_ready(self):
-        self.device_group.send_message(DeviceType.SHUTTER_CONTROL, 0, Command.SET_LED, b'\x00\xff\x00')
 
     def on_enter_shutter_open(self):
-        self.device_group.send_message(DeviceType.SHUTTER_CONTROL, 0, Command.SET_LED, b'\x00\x00\xff')
+        print("shutter led green")
+        # TODO
+
+    def on_enter_shutter_close(self):
+        print("shutter led blue")
+        # TODO
+
+    def on_enter_door_unlocking(self):
+        pass
+        # TODO
 
     def on_enter_error(self):
         print('There has been an error.')
